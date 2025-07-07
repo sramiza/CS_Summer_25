7eddba5e7e8dfcc1e92e1d7fc0648646207d4542
Syed Ramiz Abbas <syedramizabbas@gmail.com>
2025-07-07 23:42:12 +0500

    practicing with smart pointers

---
+++ Pointers/smartptr.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+int main(){
+
+    int c = 13;
+    int *ptr = &c;
+
+    unique_ptr<int> uPtr1 = make_unique<int>(42);
+    cout << uPtr1 << endl;
+    unique_ptr<int> uPtr2 = std::move(uPtr1); // for some reason the compiler prefers std:: specified
+    cout << uPtr2 << endl;
+    cout << uPtr1 << endl;
+
+    return 0;
+}
+
+// g++ -std=c++14 smartptr.cpp -o smartptr
+// ./smartptr
